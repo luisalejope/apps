b224df1206b4d65f50bd5677aa63fa5e89ee8689
pineapplexpres <luisalejandropena425@gmail.com>
2020-05-23 09:42:12 -0500

    apps

--- nodejs-app/src/routes/index.js
+++ nodejs-app/src/routes/index.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/',(req,res)=> res.send('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png" /><br /><h1>Aplicación de NodeJs corriendo modificada desde el pipeline</h1>'));
+router.get('/',(req,res)=> res.send('<h1>Aplicación de NodeJs mas chingona del mundo</h1>'));
 
 module.exports = router;
--- react-app/my-react-app/src/App.js
+++ react-app/my-react-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aplicacion web de react corriendo modificada desde el pipeline
+          Bienvenido a la aplicacion mas chingona
         </p>
       </header>
     </div>
